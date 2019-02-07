4cebb119dae5fab6211c5c0eddf9b11acf48f7cf
nevermore1258 <42537373+nevermore1258@users.noreply.github.com>
2019-02-07 16:53:22 +0700

    TAO

--- install.sh
+++ install.sh
@@ -104,6 +104,25 @@ echo "/usr/sbin/nologin" >> /etc/shells
 service ssh restart
 service dropbear restart
 
+# Install Vnstat
+apt-get -y install vnstat
+vnstat -u -i eth0
+sudo chown -R vnstat:vnstat /var/lib/vnstat
+
+
+# Install Vnstat GUI
+cd /home/vps/public_html/
+wget http://www.sqweek.com/sqweek/files/vnstat_php_frontend-1.5.1.tar.gz
+tar xf vnstat_php_frontend-1.5.1.tar.gz
+rm vnstat_php_frontend-1.5.1.tar.gz
+mv vnstat_php_frontend-1.5.1 vnstat
+cd vnstat
+sed -i "s/\$iface_list = array('eth0', 'sixxs');/\$iface_list = array('eth0');/g" config.php
+sed -i "s/\$language = 'nl';/\$language = 'en';/g" config.php
+sed -i 's/Internal/Internet/g' config.php
+sed -i '/SixXS IPv6/d' config.php
+cd
+
 # install squid3
 cd
 apt-get -y install squid3
